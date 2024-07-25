929537bc9b91cdb7ef7701455ea6a70381fbb120
Angry_Cookie <fanby2014@126.com>
2024-07-25 16:08:55 +0800

    feature : 完成回调改造

---
+++ LearnOpenGL/CLight.cpp
@@ -0,0 +1,6 @@
+#include "CLight.h"
+
+CLight::CLight(const std::string& vPath, std::shared_ptr<CShader> vShader) : CObject(vPath, vShader)
+{
+	
+}
--- LearnOpenGL/CObject.h
+++ LearnOpenGL/CObject.h
@@ -21,9 +21,21 @@
 
 class CObject
 {
-private:
+protected:
+	Eigen::Matrix4f m_Position = Eigen::Matrix4f::Identity();
+	float m_Scale = 1.0f;
+
+	// 成员变量，存储 std::function
+	std::function<void(std::chrono::duration<double>, CObject&)> m_UpdateMoveFunction;
+
+	// 检测 myFunction 是否为空
+	bool __isFunctionSet() const {
+		return static_cast<bool>(m_UpdateMoveFunction);
+	}
+
 	std::map<std::shared_ptr<CVertexArrayObject>, std::shared_ptr<CShader>> m_VAOs;
 	std::chrono::steady_clock::time_point m_Start = std::chrono::high_resolution_clock::now();
+	void __transform(std::shared_ptr<CShader> vShader);
 	void __rotation(std::shared_ptr<CShader> vShader);
 	std::vector<unsigned int> __createOffset(unsigned int vType);
 	std::vector<float> __readFloatArrayFromFile(std::ifstream& vFile);
@@ -37,5 +49,10 @@ public:
 	void setVAO(std::map<std::shared_ptr<CVertexArrayObject>, std::shared_ptr<CShader>>&& vVAOs);
 	std::map<std::shared_ptr<CVertexArrayObject>, std::shared_ptr<CShader>>::iterator begin();
 	std::map<std::shared_ptr<CVertexArrayObject>, std::shared_ptr<CShader>>::iterator end();
+	void setUpdateMoveFunction(std::function<void(std::chrono::duration<double>, CObject&)> vFunction);
+	void move(Eigen::Vector3f vDisplacement);
+	void setPosition(Eigen::Vector3f vNewPosition);
+	void scale(float vScale);
+	void setScale(float vScale);
 	void render();
 };
